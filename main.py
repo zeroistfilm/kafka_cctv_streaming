fc730b7d6743aa5761efd01b05f9fd59019ee6eb
YD <zeroistfilm@naver.com>
2022-09-03 00:44:10 +0900

    fix config for kafka

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ app = FastAPI()
 
 
 def kafkastream():
-    consumer = KafkaConsumer('my-topic', bootstrap_servers='3.38.136.70:9093')
+    consumer = KafkaConsumer('my-topic', bootstrap_servers='ec2-3-38-136-70.ap-northeast-2.compute.amazonaws.com:29092')
     try:
         for message in consumer:
             yield (b'--frame\r\n'b'Content-Type: image/jpeg\r\n\r\n' + message.value + b'\r\n\r\n')
