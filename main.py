977b2490353fe17d86b75c4b481b0230da27ca13
YD <zeroistfilm@naver.com>
2022-09-05 16:52:33 +0900

    fix config for kafka

--- main.py
+++ main.py
@@ -1,6 +1,9 @@
+import asyncio
+
 from fastapi import FastAPI
 from kafka import KafkaConsumer
 from starlette.responses import StreamingResponse
+from fastapi import FastAPI, WebSocket, Request
 
 app = FastAPI()
 
@@ -13,6 +16,22 @@ def kafkastream():
     finally:
         print('consumer close')
         consumer.close()
+
+# 웹소켓 설정 ws://127.0.0.1:8000/ws 로 접속할 수 있음
+@app.websocket("/ws")
+async def wsConnect(websocket: WebSocket):
+    print(f"client connected : {websocket.client}")
+    await websocket.accept() # client의 websocket접속 허용
+    await websocket.send_text(f"Welcome client : {websocket.client}")
+    while True:
+        #data = await websocket.receive_text()  # client 메시지 수신대기
+        #print(f"message received : {data} from : {websocket.client}")
+        await asyncio.sleep(1)
+        await websocket.send_text(f"Message text was: ") # client에 메시지 전달
+
+
+
+
 
 @app.get('/')
 async def Home():
---
+++ ws_client.py
@@ -0,0 +1,20 @@
+import asyncio
+import websockets
+
+
+async def my_connect():
+    async with websockets.connect("ws://localhost:8080/ws") as websocket:
+        try:
+            while True:
+                # if websockets.exceptions.ConnectionClosedOK:
+                #    break
+                data_rcv = await websocket.recv()
+                print("data received from server : " + data_rcv);
+        except websockets.exceptions.ConnectionClosedOK as e:
+            print(e)
+        finally:
+            await websocket.close()
+
+
+# connect to server
+asyncio.get_event_loop().run_until_complete(my_connect())
